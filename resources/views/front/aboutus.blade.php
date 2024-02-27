cf5276826a93997c5a30681f48269be0d859ad4b
RAAD <mabounkoungou001@st.ug.edu.gh>
2024-02-27 16:11:05 +0000

    changes

--- resources/views/front/aboutus.blade.php
+++ resources/views/front/aboutus.blade.php
@@ -119,6 +119,162 @@ a:hover, a:active {
 </style>
     <title>AboutUs</title>
 </head>
+<!DOCTYPE html>
+<html class="no-js" lang="en_AU" />
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>LARA-GIGS</title>
+	<meta name="description" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no" />
+	<meta name="HandheldFriendly" content="True" />
+	<meta name="pinterest" content="nopin" />
+	<meta name="csrf-token" content="{{ csrf_token() }}" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.27.3/ui/trumbowyg.min.css" integrity="sha512-Fm8kRNVGCBZn0sPmwJbVXlqfJmPC13zRsMElZenX6v721g/H7OukJd8XzDEBRQ2FSATK8xNF9UYvzsCtUpfeJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}" />
+	<link rel="shortcut icon" href="{{ asset('assets/fav/favicon.png') }}" type="image/x-icon">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+<style>
+
+:root {
+    --primary: hsla(172, 80%, 15%, 0.897);;
+    --secondary: #114d5c;
+    --light: #F0F6FF;
+    --dark: #074549;
+
+}
+
+/*** Footer ***/
+.footer {
+    margin-top: 6rem;
+    padding-top: 9rem;
+    background:
+	url({{ asset('assets/img/bg-circle.png') }}),
+	url({{ asset('assets/img/bg-triangle.png') }}),
+	url({{ asset('assets/img/bg-top.png') }}),linear-gradient(to bottom right, var(--primary), var(--secondary));
+    background-position:
+        left 0px bottom 0px,
+        right 0px top 50%,
+        center top;
+    background-repeat: no-repeat;
+}
+
+.footer .btn.btn-social {
+    margin-right: 5px;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: var(--light);
+    border: 1px solid rgba(256, 256, 256, .1);
+    border-radius: 40px;
+    transition: .3s;
+}
+
+.footer .btn.btn-social:hover {
+    color: var(--primary);
+}
+
+.footer .btn.btn-link {
+    display: block;
+    margin-bottom: 10px;
+    padding: 0;
+    text-align: left;
+    color: var(--light);
+    font-weight: normal;
+    transition: .3s;
+}
+
+.footer .btn.btn-link::before {
+    position: relative;
+    content: "\f105";
+    font-family: "Font Awesome 5 Free";
+    font-weight: 900;
+    margin-right: 10px;
+}
+
+.footer .btn.btn-link:hover {
+    letter-spacing: 1px;
+    box-shadow: none;
+}
+
+.footer .copyright {
+    padding: 25px 0;
+    font-size: 14px;
+    border-top: 1px solid rgba(256, 256, 256, .1);
+}
+
+.footer .copyright a {
+    color: var(--light);
+}
+
+.footer .footer-menu a {
+    margin-right: 15px;
+    padding-right: 15px;
+    border-right: 1px solid rgba(255, 255, 255, .1);
+}
+
+.footer .footer-menu a:last-child {
+    margin-right: 0;
+    padding-right: 0;
+    border-right: none;
+}
+
+
+	</style>
+</head>
+<body data-instant-intensity="mousedown">
+<header>
+	<nav class="navbar navbar-expand-lg navbar-light bg-white shadow py-3" style="position: fixed; top: 0; width: 100%; z-index: 1000; padding-bottom: 70px;">
+		<div class="container">
+			<a class="navbar-brand" href="{{ route('home') }}" style="color: hsla(172, 80%, 15%, 0.897);">
+				LARA-GIGS
+			</a>
+						<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ms-0 ms-sm-0 me-auto mb-2 mb-lg-0 ms-lg-4">
+					<li class="nav-item">
+						<a class="btn btn-primary" aria-current="page" href="{{ route('home') }}"  style="margin-right: 5px;">
+							<i class="fas fa-home"></i> Home
+						</a>
+											</li>
+					<li class="nav-item">
+						<a class="btn btn-primary" aria-current="page" href="{{ route('jobs') }}"  style="margin-right: 5px;">
+							<i class="fas fa-search"></i> Find Jobs
+						</a>
+											</li>
+											<li class="nav-item">
+												<a class="btn btn-primary" href="{{ route('account.createJob') }}" type="submit">
+													<i class="fas fa-plus-circle"></i> Post a Job
+												</a></li>
+				</ul>
+
+				@if (!Auth::check())
+				<a class="btn btn-outline-primary me-2" href="{{ route('account.login') }}" type="submit">
+					<i class="fas fa-sign-in-alt"></i> Login
+				</a>
+				<a class="btn btn-outline-primary me-2" href="{{ route('account.registration') }}">
+					<i class="fas fa-sign-in-alt"></i> Register
+				</a>
+
+								@else
+					@if (Auth::user()->role == 'admin')
+					<a class="btn btn-outline-primary me-2" href="{{ route('admin.dashboard') }}" type="submit">Admin</a>
+					@endif
+					<a class="btn btn-outline-primary me-2" href="{{ route('account.profile') }}" type="submit">Account</a>
+				@endif
+
+
+							</div>
+		</div>
+	</nav>
+	<br><br>
+
+</header>
 <body>
     <div class="aboutus-section">
         <div class="container">
