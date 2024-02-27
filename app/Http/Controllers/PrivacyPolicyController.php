1bcf60d13c264325b6ac2760275d021f3eea6b1b
RAAD <mabounkoungou001@st.ug.edu.gh>
2024-02-27 16:21:16 +0000

    Creating and implementing Function

--- app/Http/Controllers/PrivacyPolicyController.php
+++ app/Http/Controllers/PrivacyPolicyController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class PrivacyPolicyController extends Controller
 {
-    //
+    public function privacyPolicy(){
+        return view('front.privacypolicy');
+    }
 }
