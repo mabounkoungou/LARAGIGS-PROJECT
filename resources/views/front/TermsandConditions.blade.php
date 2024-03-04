45d9b46ae691f5d3d9ffb2ed4d50e9dcd07bd1f8
RAAD <mabounkoungou001@st.ug.edu.gh>
2024-03-04 20:34:49 +0000

    Adding Favicon in T&c page

--- resources/views/front/TermsandConditions.blade.php
+++ resources/views/front/TermsandConditions.blade.php
@@ -12,6 +12,8 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 
     <link href="css/custom.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href="{{ asset('assets/fav/favicon.png') }}" type="image/x-icon">
+
 
 
     <meta property="og:type" content="website" />
