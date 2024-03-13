e5256c7b0cdd83b7d2799b11366ba4a8ff0af617
Ghost Writer <mabounkoungou@st.ug.edu.gh>
2024-03-13 23:45:18 +0000

    update

--- resources/views/email/forgot-password.blade.php
+++ resources/views/email/forgot-password.blade.php
@@ -5,14 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Forgot Password Email</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+            color: #333;
+        }
+        .container {
+            max-width: 600px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            color: #007bff;
+        }
+        p {
+            margin-bottom: 20px;
+        }
+        a {
+            display: inline-block;
+            background-color: #007bff;
+            color: #fff;
+            text-decoration: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+        }
+        a:hover {
+            background-color: #0056b3;
+        }
+    </style>
 </head>
 <body>
-    <h1>Hello {{ $mailData['user']->name }}</h1>
+    <div class="container">
+        <h1>Hello {{ $mailData['user']->name }},</h1>
 
-    <p>Click below to change your password.</p>
-    <a href="{{ route("account.resetPassword",$mailData['token']) }}">Click Here</a>
+        <p>Click below to change your password:</p>
+        <a href="{{ route("account.resetPassword",$mailData['token']) }}">Reset Password</a>
 
-    <p>Thanks</p>
-
+        <p>Thanks</p>
+    </div>
 </body>
 </html>
